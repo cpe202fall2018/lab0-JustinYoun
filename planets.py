6ae6a467e3b8325ff55460f601673bd034ccd659
JustinYoun <43148491+JustinYoun@users.noreply.github.com>
2018-09-24 08:55:41 -0700

    Update planets.py

--- planets.py
+++ planets.py
@@ -2,10 +2,13 @@
 # CPE 202-1/2
 # Lab 0 Parkinson
 def weight_on_planets():
+    # Prompts user for weight on earth
     userWeight = float(input("What do you weigh on earth? "))
     print()
+    # Gets the weight of the user on mars and jupiter
     marsWeight = userWeight * 0.38
     jupiterWeight = userWeight * 2.34
+    # Print the final weight statement
     print("On Mars you would weigh "+str(marsWeight) + " pounds.\nOn Jupiter you would weigh "+str(jupiterWeight) + " pounds.")
 if __name__ == '__main__':
     weight_on_planets()
